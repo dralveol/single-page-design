76613873beed54fbaeb0d3fe815e8de0deda1333
Saad <bounhar.saad@gmail.com>
2023-04-23 00:47:38 +0000

    Carousel done

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,5 +1,5 @@
 //Position index
-let position = -2;
+let position = 0;
 //Arrows selection
 const right = document.querySelector("#right");
 const left = document.querySelector("#left");
@@ -8,18 +8,20 @@ const imgs = Array.from(document.querySelectorAll(".work-section__slider img"));
 
 //On click event
 left.addEventListener("click", function(e){
-	if(position<2) {
+	if(position<0) {
 		position += 1
 		imgs.forEach(function(element){
-			element.style.transform = `translateX(${position * 65}vw)`}) 
+			let imageWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--image-width')) + 15;
+			element.style.transform = `translateX(${position * imageWidth}px)`}) 
 	}
 });
 
 right.addEventListener("click", function(e){
-	if(position>-2) {
-		position -= 1
+	if(position>-4) {
+		position -= 1;
 		imgs.forEach(function(element){
-			element.style.transform = `translateX(${position * 65}vw)`}) 
+			let imageWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--image-width')) + 15;
+			element.style.transform = `translateX(${position * imageWidth}px)`}) 
 	}
 });
 
